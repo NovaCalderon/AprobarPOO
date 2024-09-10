68aef164715eed0865724373fe43998ef484586f
novacalderon <jostindcalderon@gmail.com>
2024-09-10 11:35:16 -0500

    Update Hotel.java

--- src/main/java/com/mycompany/hotel/Hotel.java
+++ src/main/java/com/mycompany/hotel/Hotel.java
@@ -2,6 +2,6 @@ package com.mycompany.hotel;
 public class Hotel {
     public static void main(String[] args) {
         System.out.println("Ya tilines, a chambear dijo Coronel");
-        System.out.println("Prueba num2");
+        System.out.println("Prueba num34");
     }
 }
