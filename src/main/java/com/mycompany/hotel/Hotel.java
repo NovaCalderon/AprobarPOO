bdc5fcf3b4ef04d55c7b8b1dc8104bbdc1580f41
novacalderon <jostindcalderon@gmail.com>
2024-09-10 10:29:35 -0500

    Update Hotel.java

--- src/main/java/com/mycompany/hotel/Hotel.java
+++ src/main/java/com/mycompany/hotel/Hotel.java
@@ -1,12 +1,6 @@
 package com.mycompany.hotel;
 public class Hotel {
     public static void main(String[] args) {
-        System.out.println("al delegado lo aprobaron y lo probaron");
-        System.out.println("la fè me aprobara");
-        System.out.println("Enfrien a Huarote");
-        System.out.println("Ingeniero completo?");
-        System.out.println("La vallejizacion de la UNI");
-        System.out.println("terrible lo que pasará en la semana 4");
-        System.out.println("Go retiro?");
+        System.out.println("Ya tilines, a chambear dijo Coronel");
     }
 }
